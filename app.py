6aaa14cd1514da05a106e24cba57b500693ada79
Vlad Ciobanu <95963142+vl3c@users.noreply.github.com>
2024-03-12 22:02:24 +0200

    Add logging functionality and create a Flask app factory

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+import logging
+import os
+from datetime import datetime
 from flask import Flask, json, request, render_template
 from static.openai_api import OpenAIChatCompletionsAPI
 
+def get_log_file_name():
+    return datetime.now().strftime('mathud_log_%y_%m_%d_%H_%M_%S.log')
 
-app = Flask(__name__)
-openai_api = OpenAIChatCompletionsAPI()
+def set_up_logging():
+    if not os.path.exists('./logs/'):
+        os.makedirs('./logs/')
+    logging.basicConfig(filename='./logs/' +  get_log_file_name(), level=logging.INFO, format='%(asctime)s %(message)s')
+
+def log_user_message(user_message):
+    user_message_json = json.loads(user_message)
+    if "canvas_state" in user_message_json:
+        canvas_state = user_message_json["canvas_state"]
+        logging.info(f'### Canvas state: {canvas_state}')
+    if "previous_results" in user_message_json:
+        previous_results = user_message_json["previous_results"]
+        logging.info(f'### Previously calculated results: {previous_results}')
+    if "user_message" in user_message_json:
+        user_message = user_message_json["user_message"]
+        logging.info(f'### User message: {user_message}')
 
 def jsonify_tool_calls(tool_calls):
     simple_tool_calls = []
@@ -14,19 +33,30 @@ def jsonify_tool_calls(tool_calls):
         simple_tool_calls.append(simple_tool_call)
     return simple_tool_calls
 
-@app.route('/')
-def get_index():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
+    set_up_logging()
+    openai_api = OpenAIChatCompletionsAPI()
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    message = request.json.get('message')
-    response = openai_api.create_chat_completion(message)
-    ai_message = response.content or ""
-    ai_tool_calls = response.tool_calls or []
-    ai_tool_calls = jsonify_tool_calls(ai_tool_calls)
-    response = json.dumps({"ai_message": ai_message, "ai_tool_calls": ai_tool_calls})
-    return response
+    @app.route('/')
+    def get_index():
+        return render_template('index.html')
+
+    @app.route('/send_message', methods=['POST'])
+    def send_message():
+        message = request.json.get('message')
+        log_user_message(message)
+        response = openai_api.create_chat_completion(message)
+        ai_message = response.content or ""
+        logging.info(f'### AI response: {ai_message}')
+        ai_tool_calls = response.tool_calls or []
+        ai_tool_calls = jsonify_tool_calls(ai_tool_calls)
+        logging.info(f'### AI tool calls: {ai_tool_calls}')
+        response = json.dumps({"ai_message": ai_message, "ai_tool_calls": ai_tool_calls})
+        return response
+    
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
