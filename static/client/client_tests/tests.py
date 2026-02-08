9b192438c76654d14625f94072be4bd3a7c32591
vl3c <vl3c@users.noreply.github.com>
2026-02-08 19:42:38 +0200

    test(client): register drawable/workspace test classes in Brython runner

--- static/client/client_tests/tests.py
+++ static/client/client_tests/tests.py
@@ -12,7 +12,10 @@ from .test_circle import TestCircle
 from .test_circle_arc import TestCircleArc
 from .test_custom_drawable_names import TestCustomDrawableNames
 from .test_drawable_dependency_manager import TestDrawableDependencyManager
-from .test_drawable_manager import TestDrawableManagerRegionLookup
+from .test_drawable_manager import (
+    TestDrawableManagerRegionLookup,
+    TestDrawableManagerColoredAreaDelegation,
+)
 from .test_drawable_name_generator import TestDrawableNameGenerator
 from .test_drawables_container import TestDrawablesContainer
 from .test_ellipse import TestEllipse
@@ -338,6 +341,7 @@ class Tests:
             TestWorkspaceSegmentPersistence,
             TestWorkspaceLabelRestore,
             TestWorkspaceManagerHelperMethods,
+            TestWorkspaceManagerOrchestration,
             TestWorkspacePlotsRestore,
             TestMathFunctions,
             TestNumberTheory,
@@ -435,6 +439,8 @@ class Tests:
             TestWindowMocks,
             TestCanvasEventHandlerTouch,
             TestDrawableDependencyManager,
+            TestDrawableManagerRegionLookup,
+            TestDrawableManagerColoredAreaDelegation,
             TestDrawablesContainer,
             TestFunctionBoundedColoredAreaIntegration,
             TestLinearAlgebraUtils,
